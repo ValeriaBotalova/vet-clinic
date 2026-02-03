adfee856ae1d1e4969cb4712775d79abf8eb34af
ValeriaBotalova <kaleriya.sych@mail.ru>
2026-02-04 01:25:21 +0300

    создание бд

--- vet_clinic/main/models.py
+++ vet_clinic/main/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилия', max_length=100)
+    phone = models.CharField('Номер телефона', max_length=20)
+    email = models.EmailField('Электронная почта', max_length=254)
+    def __str__(self):
+        return f'{self.last_name} {self.name}'
+
+class Doctor(models.Model):
+    name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилия', max_length=100)
+    specialization = models.CharField('Профиль', max_length=100)
+    def __str__(self):
+        return f'{self.last_name} {self.name}'
+
+class Pet(models.Model):
+    name = models.CharField('Имя питомца', max_length=100)
+    class Type(models.TextChoices):
+        CAT = 'cat', 'Кошка'
+        DOG = 'dog', 'Собака'
+        BIRD = 'bird', 'Птица'
+        BUNNY = 'bunny', 'Кролик'
+
+    type = models.CharField(
+        max_length=20,
+        choices=Type.choices
+    )
+    birth_date = models.DateField()
+    class Gender(models.TextChoices):
+        FEMALE = 'female', 'Женский'
+        MALE = 'male', 'Мужской'
+
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices
+    )
+    owner = models.ForeignKey(
+        Owner,
+        on_delete=models.CASCADE,
+        related_name='pets'
+    )
+    def __str__(self):
+        return f'{self.get_type_display()} {self.name} {self.birth_date}'
+
+class Appointment(models.Model):
+    pet = models.ForeignKey(
+        Pet,
+        on_delete=models.CASCADE,
+        related_name='appointments'
+    )
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    appointment_date = models.DateField('Дата приема')
+    appointment_time = models.TimeField('Время приема')
+    reason = models.CharField('Причина визита', max_length=255)
+    def __str__(self):
+        return f'{self.pet.name} — {self.appointment_date} {self.appointment_time}'
+
+
--- vet_clinic/main/views.py
+++ vet_clinic/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Главная страница</h1>")
+    return render(request, 'main/main_page.html')
+
+def appointment(request):
+    return render(request, 'main/appointment.html')
